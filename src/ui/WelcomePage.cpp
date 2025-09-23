4148ebcd6984c02818d5f1b12fc9bbea8f105984
Mitchell Richardson <sitback0706@outlook.com>
2025-09-23 14:15:06 +1000

    Added browse and remove functionalities if an instance is detected

--- src/ui/WelcomePage.cpp
+++ src/ui/WelcomePage.cpp
@@ -2,8 +2,12 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QComboBox>
+#include <QDir>
+#include <QPushButton>
+#include <iostream>
+#include <QDesktopServices>
 
-
+#include "MainWizard.h"
 
 WelcomePage::WelcomePage(QWidget *parent)
     : QWizardPage(parent)
@@ -12,11 +16,43 @@ WelcomePage::WelcomePage(QWidget *parent)
     auto* layout = new QVBoxLayout();
     setLayout(layout);
 
-    auto* usage = new QLabel("Modsync is a platform distributed by your server owner"
+    const auto installDir = QDir(QDir::homePath() + "/.minecraft/modsyncprofile");
+    std::cout << installDir.filesystemAbsolutePath().string() << '\n';
+
+    if (!installDir.exists())
+    {
+        auto* welcome = new QLabel("Modsync is a platform distributed by your server owner"
                              " to steamline the <b>install and updating</b> of mods."
                              " Having trouble connecting? <b>Try re-running Modsync.</b>");
-    usage->setWordWrap(true);
-    usage->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+        welcome->setWordWrap(true);
+        welcome->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
-    layout->addWidget(usage);
+        layout->addWidget(welcome);
+    } else
+    {
+        auto* welcome = new QLabel("It seems you already have an instance of Modsync installed."
+                                 " Pressing next will update your mods.");
+        welcome->setWordWrap(true);
+        welcome->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+
+        auto* removeButton = new QPushButton("Remove Current Instance");
+        connect(removeButton, &QPushButton::pressed, this, []()
+        {
+            QDir dir(QDir::homePath() + "/.minecraft/modsyncprofile");
+            dir.removeRecursively();
+        });
+
+        auto* browseButton = new QPushButton("Browse...");
+        connect(browseButton, &QPushButton::pressed, this, []()
+        {
+            QString dir(QDir::homePath() + "/.minecraft/modsyncprofile");
+            QDesktopServices::openUrl(QUrl::fromLocalFile(dir));
+        });
+
+        layout->addWidget(welcome);
+        layout->addWidget(removeButton);
+        layout->addWidget(browseButton);
+    }
+
+
 }
