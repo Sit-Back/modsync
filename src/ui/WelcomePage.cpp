88048dbaee8e939dbaa0867226a8206ba0c3e75a
Mitchell Richardson <sitback0706@outlook.com>
2025-09-23 13:41:10 +1000

    Updated welcome text

--- src/ui/WelcomePage.cpp
+++ src/ui/WelcomePage.cpp
@@ -12,13 +12,11 @@ WelcomePage::WelcomePage(QWidget *parent)
     auto* layout = new QVBoxLayout();
     setLayout(layout);
 
-    auto* usage = new QLabel("Select an existing server to update or create a new one through the dropdown.");
+    auto* usage = new QLabel("Modsync is a platform distributed by your server owner"
+                             " to steamline the <b>install and updating</b> of mods."
+                             " Having trouble connecting? <b>Try re-running Modsync.</b>");
     usage->setWordWrap(true);
     usage->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
-    auto* input = new QComboBox();
-    input->addItem("Create new server entry…");
-
     layout->addWidget(usage);
-    layout->addWidget(input);
 }
