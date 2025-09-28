65bbffb5bbba2deb0d466f2656d0e5e43bd75865
Mitchell Richardson <sitback0706@outlook.com>
2025-09-28 15:42:20 +1000

    Added profile string generation

--- src/ui/FinishingUpPage.cpp
+++ src/ui/FinishingUpPage.cpp
@@ -17,6 +17,18 @@ void FinishingUpPage::initializePage()
 {
     SyncClient::SyncMetadata metadata = syncer.getMetadata();
 
+    profileString = R"({
+        "modsync": {
+            "lastUsed": "1970-01-02T00:00:00.000Z",
+            "lastVersionId": "%1",
+            "created": "1970-01-02T00:00:00.000Z",
+            "name": "fabric-loader-1.21.8",
+            "icon": "Dirt",
+            "type": "custom"
+        }
+    })";
+    profileString = profileString.arg(metadata.loaderID);
+
     auto* usage = new QLabel(
         "Your server owner has recommended that you download"
         " the <b>" + metadata.loaderName + "</b>"
--- src/ui/FinishingUpPage.h
+++ src/ui/FinishingUpPage.h
@@ -14,5 +14,6 @@ public:
 
 private:
     SyncClient& syncer;
+    QString profileString;
 };
 
